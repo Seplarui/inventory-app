adc95857265436f69040e74418277fc880037f09
Sebas <seplarui@gmail.com>
2017-08-22 17:54:16 +0200

    products-list.component.ts

---
+++ src/app/products-list/products-list.component.ts
@@ -0,0 +1,51 @@
+import { Component,
+  EventEmitter,
+  Input,
+  Output
+} from '@angular/core';
+
+  import { Product } from '../product.model';
+
+  /**
+   *
+   * @ProductsList: A component for rendering all ProductRows and
+   * storing the currently selected Product
+   *
+   */
+@Component({
+  selector: 'products-list',
+  templateUrl: './products-list.component.html',
+  //styleUrls: ['./products-list.component.css']
+})
+export class ProductsListComponent {
+
+  /**
+   * @input productList - the Product [] passed to us
+   */
+
+   @Input() productList: Product[];
+
+   /**
+    * @output onProdutcSelected - outputs the current
+    *   Product whenever a new Product is selected
+    */
+
+    @Output() onProductSelected: EventEmitter<Product>;
+/**
+ * @property currentProduct - local state containing
+ * the currently selected `Product`
+ */
+
+ private currentProduct: Product;
+  constructor() {
+
+    this.onProductSelected= new EventEmitter();
+
+
+   }
+
+
+  ngOnInit() {
+  }
+
+}
